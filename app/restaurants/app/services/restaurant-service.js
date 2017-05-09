809d8ec9c45e9f33f05e35665043d17d771ce172
Adnan Lapendic <adnan.lapendic@gmail.com>
2017-05-09 22:37:10 +0200

    add footer

--- app/restaurants/app/services/restaurant-service.js
+++ app/restaurants/app/services/restaurant-service.js
@@ -2,66 +2,56 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
 
-      reservationResponse: null,
+  reservationResponse: null,
 
-      getAllRestaurants() {
-        return Ember.$.ajax({
-          url: '/api/v1/restaurant',
-          method: 'GET',
-        }).then(function(data) {
+  getAllRestaurants() {
+    return Ember.$.ajax({
+      url: '/api/v1/restaurant',
+      method: 'GET',
+    }).then(function(data) {
 
-          return data;
-        });
+      return data;
+    });
+  },
+
+  getRestaurantLocations() {
+    return Ember.$.ajax({
+      url: '/api/v1/getRestaurantsLocations',
+      method: 'GET',
+    }).then(function(data) {
+      return data;
+    });
+
+  },
+
+  getSingleRestaurant(id) {
+    return Ember.$.ajax({
+      url: "/api/v1/getRestaurantDetails",
+      method: "POST",
+      data: {
+        id: id
       },
+    }).then(function(resp) {
+      return resp;
+    }).catch(function(error) {
+      alert("Error")
+    });
 
-      getRestaurantLocations() {
-        return Ember.$.ajax({
-          url: '/api/v1/getRestaurantsLocations',
-          method: 'GET',
-        }).then(function(data) {
-          return data;
-        });
+  },
 
+  checkReservationAvailability(people, date, time, restaurantId) {
+    return Ember.$.ajax({
+      url: "/api/v1/checkForReservation",
+      method: "POST",
+      data: {
+        people: people,
+        date: date,
+        time: time,
+        restaurantId: restaurantId
       },
-
-      getSingleRestaurant(id) {
-        return Ember.$.ajax({
-          url: "/api/v1/getRestaurantDetails",
-          method: "POST",
-          data: {
-            id: id
-          },
-        }).then(function(resp) {
-          return resp;
-        }).catch(function(error) {
-          alert("Error")
-        });
-
+      xhrFields: {
+        withCredentials: true
       },
-
-      checkReservationAvailability(people, date, time, restaurantId) {
-        return Ember.$.ajax({
-          url: "/api/v1/checkForReservation",
-          method: "POST",
-          data: {
-            people: people,
-            date: date,
-            time: time,
-            restaurantId: restaurantId
-          },
-          xhrFields: {
-            withCredentials: true
-          },
-        }).then(function(resp) {
-            var promise = new Ember.RSVP.Promise(function(resolve, reject) {
-              debugger
-              resolve(resp);
-              // or reject
-              reject(error);
-              return promise;
-            }).catch(function(error) {
-              alert("Error")
-            });
-          },
-
-        });
+    });
+  },
+});
